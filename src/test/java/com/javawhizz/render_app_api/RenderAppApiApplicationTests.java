86efb43f4b76221a461fd5957f681c4720b58a67
Tanvir Ahmed <tanvir@kaz.com.bd>
2024-06-12 16:18:19 +0600

    first commit

---
+++ src/test/java/com/javawhizz/render_app_api/RenderAppApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javawhizz.render_app_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RenderAppApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
